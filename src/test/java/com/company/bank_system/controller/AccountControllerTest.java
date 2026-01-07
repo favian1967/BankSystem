78da6d52ace704a5f558fdd0c42167982f8c575b
favian47 <d@g>
2026-01-07 17:31:00 +0200

    accountControllerTest

---
+++ src/test/java/com/company/bank_system/controller/AccountControllerTest.java
@@ -0,0 +1,140 @@
+package com.company.bank_system.controller;
+
+import com.company.bank_system.dto.CreateAccountRequest;
+import com.company.bank_system.entity.Account;
+import com.company.bank_system.entity.User;
+import com.company.bank_system.entity.enums.Account.AccountStatus;
+import com.company.bank_system.entity.enums.Account.AccountType;
+import com.company.bank_system.entity.enums.Currency;
+import com.company.bank_system.entity.enums.User.UserRole;
+import com.company.bank_system.entity.enums.User.UserStatus;
+import com.company.bank_system.repo.AccountRepository;
+import com.company.bank_system.repo.UserRepository;
+import com.company.bank_system.service.JWTService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import tools.jackson.databind.ObjectMapper;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Testcontainers
+@SpringBootTest
+@AutoConfigureMockMvc
+class AccountControllerTest {
+    private final MockMvc mockMvc;
+    private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
+    private final JWTService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final ObjectMapper objectMapper;
+
+
+    @Autowired
+    AccountControllerTest(MockMvc mockMvc, AccountRepository accountRepository, UserRepository userRepository, JWTService jwtService, PasswordEncoder passwordEncoder, ObjectMapper objectMapper) {
+        this.mockMvc = mockMvc;
+        this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
+        this.jwtService = jwtService;
+        this.passwordEncoder = passwordEncoder;
+        this.objectMapper = objectMapper;
+    }
+
+    @Container
+    private static final PostgreSQLContainer postgres =
+            new PostgreSQLContainer(DockerImageName.parse("postgres:16-alpine")
+            );
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
+    }
+
+    private User testUser;
+    private String jwtToken;
+
+
+
+    @BeforeEach
+    void setUp() {
+        accountRepository.deleteAll();
+        userRepository.deleteAll();
+
+        testUser = new User();
+        testUser.setEmail("test@example.com");
+        testUser.setPasswordHash(passwordEncoder.encode("password123"));
+        testUser.setFirstName("Test");
+        testUser.setLastName("User");
+        testUser.setRole(UserRole.USER);
+        testUser.setCreatedAt(LocalDateTime.now());
+        testUser.setPhone("+79505551234");
+        testUser.setStatus(UserStatus.ACTIVE);
+        testUser = userRepository.save(testUser);
+
+        jwtToken = jwtService.generateToken(testUser.getEmail());
+    }
+
+
+    @Test
+    void createAccount_shouldCreateAccountSuccessfully() throws Exception {
+
+        //ARRANGE
+        CreateAccountRequest request = new CreateAccountRequest(
+                AccountType.CHECKING,
+                Currency.USD
+        );
+
+        //ACT & ASSERTIONS
+        mockMvc.perform(post("/api/accounts/add")
+                .header("Authorization", "Bearer " + jwtToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.accountNumber").isString())
+                .andExpect(jsonPath("$.accountType").value("CHECKING"))
+                .andExpect(jsonPath("$.currency").value("USD"))
+                .andExpect(jsonPath("$.balance").value(0))
+                .andExpect(jsonPath("$.status").value("ACTIVE"));
+
+        List<Account> accounts = accountRepository.findByUser(testUser);
+        assertThat(accounts).hasSize(1);
+
+        Account savedAccount = accounts.getFirst();
+
+        assertThat(savedAccount.getUser().getId()).isEqualTo(testUser.getId());
+        assertThat(savedAccount.getAccountType()).isEqualTo(AccountType.CHECKING);
+        assertThat(savedAccount.getCurrency()).isEqualTo(Currency.USD);
+        assertThat(savedAccount.getBalance()).isEqualByComparingTo(BigDecimal.ZERO);
+        assertThat(savedAccount.getStatus()).isEqualTo(AccountStatus.ACTIVE);
+        assertThat(savedAccount.getAccountNumber()).startsWith("40817");
+        assertThat(savedAccount.getAccountNumber()).hasSize(20);
+
+
+
+    }
+
+}
